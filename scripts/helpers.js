6c1f025390b5700f332abe298d46b20eacc0bfc8
drasnop <ponsard.antoine@gmail.com>
2015-06-10 15:46:26 -0700

    improved overall filtering of workers, talking more accurately of "complete participants"

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,61 +1,68 @@
 /* helper functions for enumerating the data and writing csv files */
 
-// add a convenient enumaration method to all objects
-Object.defineProperty(Object.prototype, "forEach", {
-   value: function(callback) {
-      Object.keys(this).forEach(function(key) {
-         callback(this[key]);
-      }, this)
-   }
-})
-
 // standardized filename and location for all parsing scripts
 exports.filename = function(name) {
    return batch + "/" + name + "-" + batch + ".csv";
 }
 
-// filters out all the invalid participants in this batch
-exports.validParticipants = function() {
-   var validParticipants = [];
 
-   input.forEach(function(worker) {
+// check if one worker completed the entire experiment (does not check for duplicates)
+exports.isComplete = function(worker) {
+   // filter out people who did not complete the tutorial (minimum number of steps is 14)
+   if (!worker.tutorial || Object.keys(worker.tutorial).length < 14)
+      return false;
 
-      // filter out people who did not complete the tutorial (minimum number of steps is 14)
-      if (!worker.tutorial || Object.keys(worker.tutorial).length < 12)
-         return;
+   // filter out people who did not complete the experiment
+   if (!worker.trials || Object.keys(worker.trials).length < 10)
+      return false;
 
-      // filter out people who did not complete the experiment
-      if (!worker.trials || Object.keys(worker.trials).length < 10)
-         return;
+   // filter out people who did not complete all the questionnaires
+   if (!worker.questionnaires || !worker.questionnaires.recognition || !worker.questionnaires.preference || !worker.questionnaires.demographics)
+      return false;
 
-      // filter out people who did not complete all the questionnaires
-      if (!worker.questionnaires || !worker.questionnaires.recognition || !worker.questionnaires.preference || !worker.questionnaires.demographics)
-         return;
+   return true;
+}
 
-      // check for duplicate
-      if (validParticipants.indexOf(worker) > 0)
-         return;
+// check if one worker tried to participate multiple times (does not check for validity)
+exports.isDuplicate = function(worker) {
+   var seenBefore = false;
 
-      validParticipants.push(worker);
+   for (var i in input) {
+      if (input[i].info.worker_id == worker.info.worker_id) {
+         if (seenBefore)
+            return true;
+         else
+            seenBefore = true;
+      }
+   }
+
+   return false;
+}
+
+
+// filters out all the incomplete participants in this batch
+exports.completeParticipants = function() {
+   return input.filter(function(worker) {
+      return exports.isComplete(worker);
    })
-
-   return validParticipants;
 }
 
 // indicate which participants tried to do the experiment multiple times (even if it was never valid)
+// NB: calling input.filter(isDuplicate) would cause the output to contain... duplicates!
 exports.duplicateParticipants = function() {
-   var participants = [];
+   var participantsIds = [];
+   var duplicatesIds = []
    var duplicates = [];
 
    input.forEach(function(worker) {
-
-      if (duplicates.indexOf(worker) > 0) {
-         duplicates.push(worker);
-         return;
-      }
-
-      // keep track of all the workers seen so far
-      participants.push(worker);
+      if (participantsIds.indexOf(worker.info.worker_id) > 0) {
+         // if we haven't seen this duplicate yet, add it
+         if (duplicatesIds.indexOf(worker.info.worker_id) < 0) {
+            duplicatesIds.push(worker.info.worker_id)
+            duplicates.push(worker);
+         }
+      } else
+         participantsIds.push(worker.info.worker_id);
    });
 
    return duplicates;
@@ -80,3 +87,38 @@ exports.writeCsvFile = function(filename, data) {
    })
 
 }
+
+
+
+
+/* add useful methods to all objects */
+
+
+Object.defineProperty(Object.prototype, "forEach", {
+   value: function(callback) {
+      Object.keys(this).forEach(function(key) {
+         callback(this[key]);
+      }, this)
+   }
+})
+
+Object.defineProperty(Object.prototype, "filter", {
+   value: function(test) {
+      var filtered = {};
+      Object.keys(this).forEach(function(key) {
+         if (test(this[key]))
+            filtered[key] = this[key];
+      }, this)
+      return filtered;
+   }
+})
+
+Object.defineProperty(Object.prototype, "map", {
+   value: function(transform) {
+      var mapped = [];
+      Object.keys(this).forEach(function(key) {
+         mapped.push(transform(this[key]));
+      }, this)
+      return mapped;
+   }
+})
--- scripts/workers.js
+++ scripts/workers.js
@@ -5,7 +5,7 @@ exports.filename = helpers.filename("workers");
 
 // define headers
 exports.output = [
-   ["workerId", "assignmentId", "defaults", "interface", "tutorial", "trials", "questionnaires", "valid", "duplicate"]
+   ["workerId", "assignmentId", "defaults", "interface", "tutorial", "trials", "questionnaires", "complete", "duplicate"]
 ];
 
 // parse data
@@ -28,13 +28,17 @@ input.forEach(function(worker) {
          questionnaires = "all";
    }
 
-   // "valid" if the entire experiment was completed
-   var valid = helpers.validParticipants().indexOf(worker) > 0 ? "valid" : "";
+   // "complete" if the entire experiment was completed
+   var complete = helpers.isComplete(worker) ? "complete" : "";
 
    // "duplicate" if worker participated more than once
-   var duplicate = helpers.duplicateParticipants().indexOf(worker) > 0 ? "duplicate" : "";
+   var duplicate = helpers.isDuplicate(worker) ? "duplicate" : "";
 
    exports.output.push([worker.info.worker_id, worker.info.assignment_id, worker.condition.oppositeDefaults ? "opposite" : "", worker.condition.interface,
-      tutorial, trials, questionnaires, valid, duplicate
+      tutorial, trials, questionnaires, complete, duplicate
    ]);
 });
+
+console.log(helpers.completeParticipants().map(function(p) {
+   return p.info.worker_id;
+}).sort());
