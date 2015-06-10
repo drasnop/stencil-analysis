991eb7e22ae5cb2f2480800a12ad8dab7a9e8fce
drasnop <ponsard.antoine@gmail.com>
2015-06-10 13:42:48 -0700

    workers,js write a summary of how far each worker went

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -14,6 +14,54 @@ exports.filename = function(name) {
    return batch + "/" + name + "-" + batch + ".csv";
 }
 
+// filters out all the invalid participants in this batch
+exports.validParticipants = function() {
+   var validParticipants = [];
+
+   input.forEach(function(worker) {
+
+      // filter out people who did not complete the tutorial (minimum number of steps is 14)
+      if (!worker.tutorial || Object.keys(worker.tutorial).length < 12)
+         return;
+
+      // filter out people who did not complete the experiment
+      if (!worker.trials || Object.keys(worker.trials).length < 10)
+         return;
+
+      // filter out people who did not complete all the questionnaires
+      if (!worker.questionnaires || !worker.questionnaires.recognition || !worker.questionnaires.preference || !worker.questionnaires.demographics)
+         return;
+
+      // check for duplicate
+      if (validParticipants.indexOf(worker) > 0)
+         return;
+
+      validParticipants.push(worker);
+   })
+
+   return validParticipants;
+}
+
+// indicate which participants tried to do the experiment multiple times (even if it was never valid)
+exports.duplicateParticipants = function() {
+   var participants = [];
+   var duplicates = [];
+
+   input.forEach(function(worker) {
+
+      if (duplicates.indexOf(worker) > 0) {
+         duplicates.push(worker);
+         return;
+      }
+
+      // keep track of all the workers seen so far
+      participants.push(worker);
+   });
+
+   return duplicates;
+}
+
+
 // write an entire csv file at once, from a 2D array of data
 exports.writeCsvFile = function(filename, data) {
 
--- scripts/workers.js
+++ scripts/workers.js
@@ -1,10 +1,40 @@
 /* write summary of all the workers in this batch */
+
+// generate filename
 exports.filename = helpers.filename("workers");
 
+// define headers
 exports.output = [
-   ["workerId"]
+   ["workerId", "assignmentId", "defaults", "interface", "tutorial", "trials", "questionnaires", "valid", "duplicate"]
 ];
 
+// parse data
 input.forEach(function(worker) {
-   exports.output.push([worker.info.worker_id]);
+
+   // # of tutorial steps completed, if any
+   var tutorial = worker.tutorial ? Object.keys(worker.tutorial).length : "";
+
+   // # of experiment trials completed, if any
+   var trials = worker.trials ? Object.keys(worker.trials).length : "";
+
+   // "recognition", "preference", "all"
+   var questionnaires = "";
+   if (worker.questionnaires) {
+      if (worker.questionnaires.recognition)
+         questionnaires = "recognition";
+      if (worker.questionnaires.preference)
+         questionnaires = "preference";
+      if (worker.questionnaires.demographics)
+         questionnaires = "all";
+   }
+
+   // "valid" if the entire experiment was completed
+   var valid = helpers.validParticipants().indexOf(worker) > 0 ? "valid" : "";
+
+   // "duplicate" if worker participated more than once
+   var duplicate = helpers.duplicateParticipants().indexOf(worker) > 0 ? "duplicate" : "";
+
+   exports.output.push([worker.info.worker_id, worker.info.assignment_id, worker.condition.oppositeDefaults ? "opposite" : "", worker.condition.interface,
+      tutorial, trials, questionnaires, valid, duplicate
+   ]);
 });
