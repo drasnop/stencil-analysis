71a9559dc3e5404353b76ed704ab41973a1162be
drasnop <ponsard.antoine@gmail.com>
2015-07-21 15:57:18 -0700

    minor: deal with special cases in bonus script

--- scripts/bonuses.js
+++ scripts/bonuses.js
@@ -7,6 +7,10 @@ exports.filename = helpers.filename("bonuses");
 exports.output = [];
 helpers.validParticipants().forEach(function(worker) {
 
+   // workers who encountered problems receive the bonus manually
+   if(problems[worker.id]>0 && worker.id != "48oqor59" || worker.id == "cvmxtfbn" || worker.id == "9wl1dod1")
+      return;
+
    exports.output.push({
       // general information about this worker
       "assignment_id": worker.info.assignment_id,
