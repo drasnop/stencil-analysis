240fef9684694ab15b1b42dab2eb1fe37500054e
drasnop <ponsard.antoine@gmail.com>
2015-08-06 19:01:07 -0700

    write a list of problems experienced by workers

---
+++ scripts/problems.js
@@ -0,0 +1,27 @@
+/* write a list of problems experienced by workers */
+
+exports.output = [];
+
+var count = 0;
+var workersWithProblem = {};
+input.forEach(function(worker) {
+
+   if (worker.questionnaires && worker.questionnaires.problemFeedback) {
+      exports.output.push({
+         // general information about this worker
+         "id": worker.id,
+         "timestamp": worker.info.timestamp,
+         "worker_id": worker.info.worker_id,
+         "assignment_id": worker.info.assignment_id,
+         "interface": worker.condition.interface,
+         "defaults": worker.condition.oppositeDefaults ? "opposite" : "",
+
+         "problemFeedback": helpers.formatStringForCSV(worker.questionnaires.problemFeedback)
+      })
+      count++;
+      workersWithProblem[worker.info.worker_id] = (workersWithProblem[worker.info.worker_id] ? workersWithProblem[worker.info.worker_id]++ : 1);
+   }
+})
+
+console.log(Object.keys(workersWithProblem).length + " workers experienced a problem (" + count + " total)");
+console.log();
